3b7a6d95d8abf076f3d4439458ee03c918687e08
udit <its.uditsingh.004@gmail.com>
2024-01-27 19:38:25 +0530

    add a block (finally) that always get executed.

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -7,6 +7,10 @@ paye invalid input ka'''
 try:  #suspicious piece of code
     n = int(input('Enter an integer: ')) 
     a = 7 / n
-    print(a)
-except:  #handling
-    print("Invalid input")
+    print('a = ',a)
+except Exception as p:  #handling , using Exception as we can see the miskate that is made
+    print("Invalid input.", "heres the mistake that is made:", p)
+
+finally: #this will be executed always
+    b = 7 
+    print('b = ',b)
